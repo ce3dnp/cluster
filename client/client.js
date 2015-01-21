dbee11edcdf33e8338f80381cde2d4b76001dbd4
cdh <m0atz.colin@gmail.com>
2015-01-21 22:09:01 +0000

    major updates to bootstrap and CSS

--- client/client.js
+++ client/client.js
@@ -1,9 +1,3 @@
-Meteor.startup(function() {
-  Tracker.autorun(function() {
-    console.log('There are ' + Spots.find().count() + ' spots');
-  });
-});
-
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY'
 });
--- client/templates/posts/posts.js
+++ client/templates/posts/posts.js
@@ -1,10 +1,22 @@
-Template.postsList.helpers({
+// Template.postsList.helpers({
+//   spots: function () {
+//     return Spots.find( {}, {sort: {timestamp: -1 }});
+//   },
+//   clusterCount: function () {
+//     return Spots.find().count();
+//   }
+// });
+
+Template.header.helpers({
   spots: function () {
     return Spots.find( {}, {sort: {timestamp: -1 }});
+  },
+  clusterCount: function () {
+    return Spots.find().count();
   }
 });
 
-Template.postsList.events({
+Template.header.events({
   'submit form': function(e) {
     e.preventDefault()
 
@@ -21,3 +33,65 @@ Template.postsList.events({
     spot._id = Spots.insert(spot);
   }
 });
+
+// Template.postsList.events({
+//   'submit form': function(e) {
+//     e.preventDefault()
+
+//     var spot = {
+//       dx:   $(e.target).find('[id=dxCallSign]').val().toUpperCase(),
+//       cc:   'England',
+//       freq: $(e.target).find('[id=freq]').val(),
+//       mode: $(e.target).find('[id=mode]').val(),
+//       timestamp: moment().format('DD-MM-YY HH:mm:ss ZZ'),
+//       spotter: Meteor.users.findOne().username.toUpperCase(),
+//       comments: $(e.target).find('[id=comments]').val()
+//     };
+
+//     spot._id = Spots.insert(spot);
+//   }
+// });
+
+// Template.postsList.rendered = function() {
+//   $('.filterable .btn-filter').click(function(){
+//         var $panel = $(this).parents('.filterable'),
+//         $filters = $panel.find('.filters input'),
+//         $tbody = $panel.find('.table tbody');
+//         if ($filters.prop('disabled') == true) {
+//             $filters.prop('disabled', false);
+//             $filters.first().focus();
+//         } else {
+//             $filters.val('').prop('disabled', true);
+//             $tbody.find('.no-result').remove();
+//             $tbody.find('tr').show();
+//         }
+//     });
+
+//     $('.filterable .filters input').keyup(function(e){
+//         /* Ignore tab key */
+//         var code = e.keyCode || e.which;
+//         if (code == '9') return;
+//         /* Useful DOM data and selectors */
+//         var $input = $(this),
+//         inputContent = $input.val().toLowerCase(),
+//         $panel = $input.parents('.filterable'),
+//         column = $panel.find('.filters th').index($input.parents('th')),
+//         $table = $panel.find('.table'),
+//         $rows = $table.find('tbody tr');
+//         /* Dirtiest filter function ever ;) */
+//         var $filteredRows = $rows.filter(function(){
+//             var value = $(this).find('td').eq(column).text().toLowerCase();
+//             return value.indexOf(inputContent) === -1;
+//         });
+//         /* Clean previous no-result if exist */
+//         $table.find('tbody .no-result').remove();
+//         /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
+//         $rows.show();
+//         $filteredRows.hide();
+//         /* Prepend no-result row if all rows are filtered */
+//         if ($filteredRows.length === $rows.length) {
+//             $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">No result found</td></tr>'));
+//         }
+//     });
+// };
+
